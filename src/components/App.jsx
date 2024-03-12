6a798511a349cd9f0f0621544b4b2c4cf262c436
koshmaniuk <trident93@gmail.com>
2024-03-12 02:52:55 -0400

    fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,16 @@ import "./App.css";
 import ContactList from "./ContactList/ContactList.jsx";
 import SearchBox from "./SearchBox/SearchBox.jsx";
 import ContactForm from "./ContactForm/ContactForm.jsx";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchContacts } from "../redux/operations";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
   return (
     <div className="app-container">
       <h1 className="title">Phonebook</h1>
